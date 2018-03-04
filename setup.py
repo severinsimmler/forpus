606838a9face04c522108e23d5705bb0070e2ecf
severinsimmler <severin.simmler@posteo.de>
2018-03-04 16:47:26 +0100

    Add content so far

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+setup(
+    name='forpus',
+    version='0.0.1.dev1',
+    description='Converting plain text corpora to NLP-specific corpus formats.',
+    # url
+    author='Severin Simmler',
+    author_email='severin.simmler@stud-mail.uni-wuerzburg.de',
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python :: 3',
+    ],
+    packages=['forpus']
+    # install_requires
+)
