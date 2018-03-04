e31a740c168f95d46f048b8db501888f3d319cab
severinsimmler <severin.simmler@posteo.de>
2018-03-04 18:23:59 +0100

    Extend pseudo docstring (Google style)

--- forpus/forpus.py
+++ forpus/forpus.py
@@ -3,7 +3,40 @@ from metadata_toolbox.utils import fname2metadata, metadata2fname
 import json
 
 class Corpus:
+    """The summary line for a class docstring should fit on one line.
+    
+    If the class has public attributes, they may be documented here
+    in an ``Attributes`` section and follow the same formatting as a
+    function's ``Args`` section. Alternatively, attributes may be documented
+    inline with the attribute's declaration (see __init__ method below).
+
+    Properties created with the ``@property`` decorator should be documented
+    in the property's getter method.
+
+    Attributes:
+        attr1 (str): Description of `attr1`.
+        attr2 (:obj:`int`, optional): Description of `attr2`.
+    
+    """
     def __init__(self, source, target, fname_pattern='{author}_{title}'):
+        """Example of docstring on the __init__ method.
+
+        The __init__ method may be documented in either the class level
+        docstring, or as a docstring on the __init__ method itself.
+
+        Either form is acceptable, but the two should not be mixed. Choose one
+        convention to document the __init__ method and be consistent with it.
+
+        Note:
+            Do not include the `self` parameter in the ``Args`` section.
+
+        Args:
+            param1 (str): Description of `param1`.
+            param2 (:obj:`int`, optional): Description of `param2`. Multiple
+                lines are supported.
+            param3 (:obj:`list` of :obj:`str`): Description of `param3`.
+
+        """
         def stream_corpus(path, fname_pattern):
             p = Path(path)
             for file in p.glob('*.txt'):
@@ -13,13 +46,18 @@ class Corpus:
         self.target = target
     
     def to_json(self, onefile=True):
-        """Short description.
-        
+        """Class methods are similar to regular functions.
+
+        Note:
+            Do not include the `self` parameter in the ``Args`` section.
+
         Args:
-            onefile (bool): Description.
-        
+            param1: The first parameter.
+            param2: The second parameter.
+
         Returns:
-            None.
+            True if successful, False otherwise.
+
         """
         if onefile:
             corpus_json = dict()
