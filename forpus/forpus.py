e8bbedd2d704a935f38ec6fe9e4004795de85b47
severinsimmler <severin.simmler@posteo.de>
2018-03-04 17:29:05 +0100

    Add metadata handling

--- forpus/forpus.py
+++ forpus/forpus.py
@@ -1,11 +1,12 @@
 from pathlib import Path
+from metadata_toolbox import utils as mdt
 
 class Corpus:
-    def __init__(self, source, target):
-        def stream_corpus(path):
+    def __init__(self, source, target, fname_pattern='{author}_{title}'):
+        def stream_corpus(path, fname_pattern):
             p = Path(path)
             for file in p.glob('*.txt'):
                 with file.open() as document:
-                    yield document.read()
-        self.corpus = stream_corpus(source)
+                    yield mdt.fname2metadata(file, fname_pattern), document.read()
+        self.corpus = stream_corpus(source, fname_pattern)
         self.target = target
