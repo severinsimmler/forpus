97837f24948b8b214f19e363a123e2543530ba20
severinsimmler <severin.simmler@posteo.de>
2018-03-04 18:14:03 +0100

    Add first method:

--- forpus/forpus.py
+++ forpus/forpus.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 from metadata_toolbox import utils as mdt
+import json
 
 class Corpus:
     def __init__(self, source, target, fname_pattern='{author}_{title}'):
@@ -10,3 +11,22 @@ class Corpus:
                     yield mdt.fname2metadata(str(file), fname_pattern), document.read()
         self.corpus = stream_corpus(source, fname_pattern)
         self.target = target
+    
+    def to_json(self, onefile=True):
+        if onefile:
+            corpus_json = dict()
+        for metadata, text in self.corpus:
+            stem = Path(metadata.index[0]).stem
+            document_json = metadata.to_dict('record')[0]
+            document_json['text'] = text
+            if onefile:
+                corpus_json[stem] = document_json
+            else:
+                document_json['stem'] = stem
+                p = Path(self.target, stem + '.json')
+                with p.open('w', encoding='utf-8') as file:
+                    json.dump(document_json, file)
+        if onefile:
+            p = Path(self.target, 'corpus.json')
+            with p.open('w', encoding='utf-8') as file:
+                json.dump(corpus_json, file)
