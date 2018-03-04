dd0e5009b9d877048426b0289fc6b6f982751b8a
severinsimmler <severin.simmler@posteo.de>
2018-03-04 17:50:57 +0100

    Specify mode and encoding

--- forpus/forpus.py
+++ forpus/forpus.py
@@ -6,7 +6,7 @@ class Corpus:
         def stream_corpus(path, fname_pattern):
             p = Path(path)
             for file in p.glob('*.txt'):
-                with file.open() as document:
+                with file.open('r', encoding='utf-8') as document:
                     yield mdt.fname2metadata(str(file), fname_pattern), document.read()
         self.corpus = stream_corpus(source, fname_pattern)
         self.target = target
