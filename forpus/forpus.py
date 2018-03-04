c94c0aaf9ab25a18ab3205ac70e112f0a94c9b33
severinsimmler <severin.simmler@posteo.de>
2018-03-04 17:12:25 +0100

    Add function to stream corpus

--- forpus/forpus.py
+++ forpus/forpus.py
@@ -2,4 +2,8 @@ from pathlib import Path
 
 class Corpus:
     def __init__(self, source, target):
-        
+        def stream_corpus(path):
+            p = Path(path)
+            for file in p.glob('*.txt'):
+                with file.open() as document:
+                    yield document.read()
