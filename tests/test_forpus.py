cbd6735c088c130db39c0ad8898f74943891cd73
severinsimmler <severin.simmler@posteo.de>
2018-03-14 22:01:41 +0100

    Improve tests

--- tests/test_forpus.py
+++ tests/test_forpus.py
@@ -17,19 +17,18 @@ def drop_stopwords(tokens, stopwords=['the', 'a', 'of']):
 
 class TestJSON(TestCase):
     def setUp(self):
-        pass
+        self.output = Path('output')
+        self.output.mkdir()
+        self.corpus = forpus.Corpus(source='corpus',
+                                    target='output')
 
     def test_conversion_onefile(self):
-        corpus = forpus.Corpus(source='corpus',
-                               target='output')
-        corpus.to_json(onefile=True)
+        self.corpus.to_json(onefile=True)
         generated_file = Path('output', 'corpus.json')
         self.assertTrue(generated_file.exists())
     
     def test_conversion_multiple_files(self):
-        corpus = forpus.Corpus(source='corpus',
-                               target='output')
-        corpus.to_json(onefile=False)
+        self.corpus.to_json(onefile=False)
         peter = Path('output', 'peter_doc1.json')
         paul = Path('output', 'paul_doc2.json')
         mary = Path('output', 'mary_doc3.json')
@@ -41,155 +40,123 @@ class TestJSON(TestCase):
                                fname_pattern='{author}/{title}')
         with self.assertRaises(ValueError):
             corpus.to_json()
+    
+    def tearDown(self):
+        for file in self.output.iterdir():
+            file.unlink()
+        self.output.rmdir()
 
 class TestDocumentTermMatrix(TestCase):
     def setUp(self):
         self.generated_file = Path('output', 'corpus.matrix')
         self.metadata = Path('output', 'corpus.metadata')
+        self.output = Path('output')
+        self.output.mkdir()
+        self.corpus = forpus.Corpus(source='corpus',
+                                    target='output')
 
     def test_conversion(self):
-        if self.generated_file.exists():
-            self.generated_file.unlink()
-        if self.metadata.exists():
-            self.metadata.unlink()
-
-        corpus = forpus.Corpus(source='corpus',
-                               target='output')
-        corpus.to_document_term_matrix(tokenizer=tokenizer,
-                                       counter=Counter,
-                                       drop_stopwords=drop_stopwords)
+        self.corpus.to_document_term_matrix(tokenizer=tokenizer,
+                                            counter=Counter,
+                                            drop_stopwords=drop_stopwords)
         self.assertTrue(self.generated_file.exists() and
                         self.metadata.exists())
     
     def test_third_party_tokenizer(self):
-        if self.generated_file.exists():
-            self.generated_file.unlink()
-        if self.metadata.exists():
-            self.metadata.unlink()
-
-        corpus = forpus.Corpus(source='corpus',
-                               target='output')
-        corpus.to_document_term_matrix(tokenizer=tokenize.wordpunct_tokenize,
-                                       counter=Counter,
-                                       drop_stopwords=drop_stopwords)
+        self.corpus.to_document_term_matrix(tokenizer=tokenize.wordpunct_tokenize,
+                                            counter=Counter,
+                                            drop_stopwords=drop_stopwords)
         self.assertTrue(self.generated_file.exists() and
                         self.metadata.exists())
     
     def test_third_party_counter(self):
-        if self.generated_file.exists():
-            self.generated_file.unlink()
-        if self.metadata.exists():
-            self.metadata.unlink()
-
-        corpus = forpus.Corpus(source='corpus',
-                               target='output')
-        corpus.to_document_term_matrix(tokenizer=tokenizer,
-                                       counter=FreqDist,
-                                       drop_stopwords=drop_stopwords)
+        self.corpus.to_document_term_matrix(tokenizer=tokenizer,
+                                            counter=FreqDist,
+                                            drop_stopwords=drop_stopwords)
         self.assertTrue(self.generated_file.exists() and
                         self.metadata.exists())
+    
+    def tearDown(self):
+        for file in self.output.iterdir():
+            file.unlink()
+        self.output.rmdir()
 
 class TestGraph(TestCase):
     def setUp(self):
         self.generated_file = Path('output', 'corpus.gexf')
+        self.output = Path('output')
+        self.output.mkdir()
+        self.corpus = forpus.Corpus(source='corpus',
+                                    target='output')
 
     def test_conversion(self):
-        if self.generated_file.exists():
-            self.generated_file.unlink()
-        corpus = forpus.Corpus(source='corpus',
-                               target='output')
-        corpus.to_graph(tokenizer=tokenizer,
-                        counter=Counter,
-                        variant='gexf',
-                        drop_stopwords=drop_stopwords)
+        self.corpus.to_graph(tokenizer=tokenizer,
+                             counter=Counter,
+                             variant='gexf',
+                             drop_stopwords=drop_stopwords)
         self.assertTrue(self.generated_file.exists())
     
     def test_exception(self):
-        corpus = forpus.Corpus(source='corpus',
-                               target='output')
         with self.assertRaises(ValueError):
-            corpus.to_graph(tokenizer=tokenizer,
-                            counter=Counter,
-                            variant='notsupported')
+            self.corpus.to_graph(tokenizer=tokenizer,
+                                 counter=Counter,
+                                 variant='notsupported')
 
     def test_third_party_tokenizer(self):
-        if self.generated_file.exists():
-            self.generated_file.unlink()
-
-        corpus = forpus.Corpus(source='corpus',
-                               target='output')
-        corpus.to_graph(tokenizer=tokenize.wordpunct_tokenize,
-                        counter=Counter,
-                        drop_stopwords=drop_stopwords)
+        self.corpus.to_graph(tokenizer=tokenize.wordpunct_tokenize,
+                             counter=Counter,
+                             drop_stopwords=drop_stopwords)
         self.assertTrue(self.generated_file.exists())
     
     def test_third_party_counter(self):
-        if self.generated_file.exists():
-            self.generated_file.unlink()
-
-        corpus = forpus.Corpus(source='corpus',
-                               target='output')
-        corpus.to_graph(tokenizer=tokenizer,
-                        counter=FreqDist,
-                        drop_stopwords=drop_stopwords)
+        self.corpus.to_graph(tokenizer=tokenizer,
+                             counter=FreqDist,
+                             drop_stopwords=drop_stopwords)
         self.assertTrue(self.generated_file.exists())
+
+    def tearDown(self):
+        for file in self.output.iterdir():
+            file.unlink()
+        self.output.rmdir()
 
 class TestLdaC(TestCase):
     def setUp(self):
         self.generated_file1 = Path('output', 'corpus.ldac')
         self.generated_file2 = Path('output', 'corpus.tokens')
         self.metadata = Path('output', 'corpus.metadata')
+        self.output = Path('output')
+        self.output.mkdir()
+        self.corpus = forpus.Corpus(source='corpus',
+                                    target='output')
 
     def test_conversion(self):
-        if self.generated_file1.exists():
-            self.generated_file1.unlink()
-        if self.generated_file2.exists():
-            self.generated_file2.unlink()
-        if self.metadata.exists():
-            self.metadata.unlink()
-            
-        corpus = forpus.Corpus(source='corpus',
-                               target='output')
-        corpus.to_ldac(tokenizer=tokenizer,
-                       counter=Counter,
-                       drop_stopwords=drop_stopwords)
+        self.corpus.to_ldac(tokenizer=tokenizer,
+                            counter=Counter,
+                            drop_stopwords=drop_stopwords)
         self.assertTrue(self.generated_file1.exists() and
                         self.generated_file2.exists() and
                         self.metadata.exists())
     
     def test_third_party_tokenizer(self):
-        if self.generated_file1.exists():
-            self.generated_file1.unlink()
-        if self.generated_file2.exists():
-            self.generated_file2.unlink()
-        if self.metadata.exists():
-            self.metadata.unlink()
-
-        corpus = forpus.Corpus(source='corpus',
-                               target='output')
-        corpus.to_ldac(tokenizer=tokenize.wordpunct_tokenize,
-                       counter=Counter,
-                       drop_stopwords=drop_stopwords)
+        self.corpus.to_ldac(tokenizer=tokenize.wordpunct_tokenize,
+                            counter=Counter,
+                            drop_stopwords=drop_stopwords)
         self.assertTrue(self.generated_file1.exists() and
                         self.generated_file2.exists() and
                         self.metadata.exists())
     
     def test_third_party_counter(self):
-        if self.generated_file1.exists():
-            self.generated_file1.unlink()
-        if self.generated_file2.exists():
-            self.generated_file2.unlink()
-        if self.metadata.exists():
-            self.metadata.unlink()
-
-        corpus = forpus.Corpus(source='corpus',
-                               target='output')
-        corpus.to_ldac(tokenizer=tokenizer,
-                       counter=FreqDist,
-                       drop_stopwords=drop_stopwords)
+        self.corpus.to_ldac(tokenizer=tokenizer,
+                            counter=FreqDist,
+                            drop_stopwords=drop_stopwords)
         self.assertTrue(self.generated_file1.exists() and
                         self.generated_file2.exists() and
                         self.metadata.exists())
+
+    def tearDown(self):
+        for file in self.output.iterdir():
+            file.unlink()
+        self.output.rmdir()
 
 class TestSvmLight(TestCase):
     def setUp(self):
@@ -197,50 +164,39 @@ class TestSvmLight(TestCase):
         self.generated_file1 = Path('output', 'corpus.svmlight')
         self.generated_file2 = Path('output', 'corpus.tokens')
         self.metadata = Path('output', 'corpus.metadata')
+        self.output = Path('output')
+        self.output.mkdir()
+        self.corpus = forpus.Corpus(source='corpus',
+                                    target='output')
 
     def test_conversion(self):
-        corpus = forpus.Corpus(source='corpus',
-                               target='output')
-        corpus.to_svmlight(tokenizer=tokenizer,
-                           classes=self.classes,
-                           counter=Counter,
-                           drop_stopwords=drop_stopwords)
+        self.corpus.to_svmlight(tokenizer=tokenizer,
+                                classes=self.classes,
+                                counter=Counter,
+                                drop_stopwords=drop_stopwords)
         self.assertTrue(self.generated_file1.exists() and
                         self.generated_file2.exists() and
                         self.metadata.exists())
                         
     def test_third_party_tokenizer(self):
-        if self.generated_file1.exists():
-            self.generated_file1.unlink()
-        if self.generated_file2.exists():
-            self.generated_file2.unlink()
-        if self.metadata.exists():
-            self.metadata.unlink()
-
-        corpus = forpus.Corpus(source='corpus',
-                               target='output')
-        corpus.to_svmlight(tokenizer=tokenize.wordpunct_tokenize,
-                           classes=self.classes,
-                           counter=Counter,
-                           drop_stopwords=drop_stopwords)
+        self.corpus.to_svmlight(tokenizer=tokenize.wordpunct_tokenize,
+                                classes=self.classes,
+                                counter=Counter,
+                                drop_stopwords=drop_stopwords)
         self.assertTrue(self.generated_file1.exists() and
                         self.generated_file2.exists() and
                         self.metadata.exists())
     
     def test_third_party_counter(self):
-        if self.generated_file1.exists():
-            self.generated_file1.unlink()
-        if self.generated_file2.exists():
-            self.generated_file2.unlink()
-        if self.metadata.exists():
-            self.metadata.unlink()
-
-        corpus = forpus.Corpus(source='corpus',
-                               target='output')
-        corpus.to_svmlight(tokenizer=tokenizer,
-                           classes=self.classes,
-                           counter=FreqDist,
-                           drop_stopwords=drop_stopwords)
+        self.corpus.to_svmlight(tokenizer=tokenizer,
+                                classes=self.classes,
+                                counter=FreqDist,
+                                drop_stopwords=drop_stopwords)
         self.assertTrue(self.generated_file1.exists() and
                         self.generated_file2.exists() and
                         self.metadata.exists())
+
+    def tearDown(self):
+        for file in self.output.iterdir():
+            file.unlink()
+        self.output.rmdir()
